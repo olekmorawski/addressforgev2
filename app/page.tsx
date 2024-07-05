f2d8bbee743f6e0d365d43562f07b06be4c17212
Aleksander Morawski <olek.morawski@gmail.com>
2024-07-05 18:33:51 +0200

    pages added

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
-    return (
-        <main className="h-1 w-8 bg-slate-50 text-sm">
-        </main>
-    )
-}
---
+++ app/pages/tool.tsx
@@ -0,0 +1,8 @@
+export default function Tool() {
+    return (
+        <div>
+            <h1>Tool Page</h1>
+            <p>This is the tool page.</p>
+        </div>
+    )
+}
