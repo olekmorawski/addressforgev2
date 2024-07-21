a9c6b05a1f82a39edea8f76eeda6dab885b8f3f4
Aleksander Morawski <olek.morawski@gmail.com>
2024-07-21 19:44:53 +0200

    nearly done

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,47 @@
 import Image from 'next/image'
 import { Infocards } from '@/components/Infocards'
+import Golem from '../images/golem.svg'
+import Cloud from '../images/cloudlanding.svg'
 import Link from 'next/link'
 
 export default function Home() {
     return (
-        <div className="mx-auto max-w-[1440px] overflow-hidden px-4 py-4">
-            <div className="flex flex-col items-start justify-between md:flex-row">
-                <div className="mb-8 md:mb-0 md:w-1/2">
-                    <h1 className="mb-4 text-4xl font-bold">
-                        Ethereum salt generator
-                    </h1>
-                    <p className="mb-6 text-gray-600">
-                        Efficiently create and manage Ethereum salt addresses
-                        while optimizing gas costs
-                    </p>
-                    <Link href="/tool">
-                        <span className="group relative mb-2 me-2 inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-cyan-500 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-cyan-200 group-hover:from-cyan-500 group-hover:to-blue-500">
-                            <span className="relative rounded-md bg-[#FAFAFA] px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0">
-                                Launch App
+        <div className="flex-grow bg-main">
+            <main className="mx-auto h-full max-w-[1440px] px-8 py-12">
+                <div className="flex flex-col items-start justify-between md:flex-row">
+                    <div className="mb-8 md:mb-0 md:w-1/2">
+                        <h1 className="mb-4 text-4xl font-bold">
+                            Ethereum salt generator
+                        </h1>
+                        <p className="mb-6 text-gray-600">
+                            Efficiently create and manage Ethereum salt
+                            addresses while optimizing gas costs
+                        </p>
+                        <Link href="/tool">
+                            <span className="group relative mb-2 me-2 inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-cyan-500 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-cyan-200 group-hover:from-cyan-500 group-hover:to-blue-500">
+                                <span className="relative rounded-md bg-[#FAFAFA] px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0">
+                                    Launch App
+                                </span>
                             </span>
-                        </span>
-                    </Link>
-                    <div className="mt-4 flex items-center">
-                        <span className="mr-2 text-sm text-gray-500">
-                            Powered by
-                        </span>
-                        <Image src={''} alt="Golem" width={60} height={20} />
+                        </Link>
+                        <div className="mt-4 flex items-center">
+                            <span className="mr-2 text-sm text-gray-500">
+                                Powered by
+                            </span>
+                            <Image
+                                src={Golem}
+                                alt="Golem"
+                                width={60}
+                                height={20}
+                            />
+                        </div>
+                    </div>
+                    <Image src={Cloud} alt={''} className="absolute"></Image>
+                    <div className="w-full md:w-1/2">
+                        <Infocards />
                     </div>
                 </div>
-                <div className="md:w-1/2">
-                    <Infocards />
-                </div>
-            </div>
+            </main>
         </div>
     )
 }
--- components/Infocards.tsx
+++ components/Infocards.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import {
+    Voicemail,
+    Fingerprint,
+    CurrencyEth,
+} from '@phosphor-icons/react/dist/ssr'
 
-export const Infocards = () => (
-    <div className="relative">
-        {[
-            'Create unique Ethereum addresses.',
-            'Reduce gas costs through customizing smart contract address.',
-            'Build a memorable address identity.',
-        ].map((text, index) => (
-            <div
-                key={index}
-                className="relative mb-4 h-[214px] w-[392px] rounded-lg border-2 border-black border-opacity-5 bg-black bg-opacity-5 p-4 shadow-sm transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:border-opacity-10 hover:bg-opacity-10 hover:shadow-md"
-            >
-                <p className="text-sm">{text}</p>
-            </div>
-        ))}
-    </div>
-)
+export const Infocards = () => {
+    const icons = [
+        <Voicemail size={36} className="text-blue-500" />,
+        <Fingerprint size={36} className="text-blue-500" />,
+        <CurrencyEth size={36} className="text-blue-500" />,
+    ]
+
+    return (
+        <div className="flex flex-col items-end space-y-8">
+            {[
+                'Create unique Ethereum addresses.',
+                'Reduce gas costs through customizing smart contract address.',
+                'Build a memorable address identity.',
+            ].map((text, index) => (
+                <div
+                    key={index}
+                    className="h-[200px] w-full max-w-[392px] rounded-lg border-2 border-black border-opacity-5 bg-black bg-opacity-5 p-4 shadow-sm transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:border-opacity-10 hover:bg-opacity-10 hover:shadow-md"
+                >
+                    <div className="flex h-full flex-col items-center justify-between">
+                        <div className="mt-2 flex h-12 w-12 items-center justify-center rounded-xl bg-white">
+                            {icons[index]}
+                        </div>
+                        <p className="text-center text-sm">{text}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
